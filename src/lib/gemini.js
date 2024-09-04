7eac38bda3bebc6c04893631a7b82ff52c35dd42
Emre <ahmet.emre.cakmak@yandex.com>
2024-09-04 15:50:17 +0300

    fix:img

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -12,6 +12,6 @@ const safetySettings = [
   ];const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_PUBLIC_KEY);
   
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro", safetySettings,});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySettings,});
 
 export default model;
