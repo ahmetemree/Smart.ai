3ae4e1a7633a9a926c765e8f80d63fb7621d26c6
Emre <ahmet.emre.cakmak@yandex.com>
2024-09-05 15:26:26 +0300

    chatlist update

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,59 +1,57 @@
-import { Link } from 'react-router-dom'
-import './chatList.scss'
-import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom';
+import './chatList.scss';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { useAuth } from '@clerk/clerk-react';
+
 const ChatList = () => {
   const { getToken } = useAuth();
-  const [token,setUserToken] = useState("")
-  
-  useEffect(()=>{
-    const takeToken = async ()=>{
+  const [token, setUserToken] = useState("");
+
+  useEffect(() => {
+    const takeToken = async () => {
       const takenToken = await getToken();
-      setUserToken(takenToken)
+      setUserToken(takenToken);
       console.log(takenToken);
-    }
-    takeToken()
-  },[])
-  
+    };
+    takeToken();
+  }, [getToken]);
+
   const { isPending, error, data } = useQuery({
     queryKey: ['userChats'],
     queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`,{
-        credentials:"include",
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+        credentials: "include",
         headers: {
           'Authorization': `Bearer ${token}`,
         },
-      }).then((res) =>
-        res.json(),
-      ),
-    })
-    
-    
-  return (
-    <div className='chatList' >
-        <span className='title'>Dashboard</span>
-        <Link to="/dashboard">Create a new Chat</Link>
-        <Link to="/">Explore SmartAI</Link>
-        <Link to="/" >Contact</Link>
-        <hr/>
-        <span className='title'>Recent Chats</span>
-        <div className="list">
-            {isPending ? "Loading.." : error ? "something went wrong.." : data?.map(chat=>(
-              
-              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>{chat.title}</Link>
-              ))}
-        </div>
-        <hr/>
-        <div className="upgrade">
-            <img src="/logo2.png" alt="" />
-            <div className="texts">
-                <span>Upgrade Pro Version</span>
-                <span>Get unlimited acess to all features</span>
-            </div>
-        </div>
-    </div>
-  )
-}
+      }).then((res) => res.json()),
+    enabled: !!token, // token gelene kadar sorguyu beklet
+  });
 
-export default ChatList
+  return (
+    <div className='chatList'>
+      <span className='title'>Dashboard</span>
+      <Link to="/dashboard">Create a new Chat</Link>
+      <Link to="/">Explore SmartAI</Link>
+      <Link to="/">Contact</Link>
+      <hr />
+      <span className='title'>Recent Chats</span>
+      <div className="list">
+        {isPending ? "Loading.." : error ? "something went wrong.." : data?.map(chat => (
+          <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>{chat.title}</Link>
+        ))}
+      </div>
+      <hr />
+      <div className="upgrade">
+        <img src="/logo2.png" alt="" />
+        <div className="texts">
+          <span>Upgrade Pro Version</span>
+          <span>Get unlimited access to all features</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
